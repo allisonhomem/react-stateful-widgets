678c8d8541940a61dcdebdd03b6b210b4c040aa2
Allison Homem <mrs.allisonhomem@outlook.com>
2021-08-09 14:06:37 -0500

    working on programmers component

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -23,18 +23,10 @@ export default function Squares() {
   const [activeSquare, setActiveSquare] = useState(null);
 
   const getClassName = id => {
-    // This is NOT a click handler but a helper, used inside the JSX (see below).
-    // It should return a string containing the class name of 'active', if the id passed
-    // as the argument matches the active square in state, empty string otherwise.
-    // Right-click and "inspect element" on the square to see its effect.
     return (activeSquare===id) ? 'active' :'';
   };
 
   const markActive = id => {
-    // This is a helper used inside an _inlined_ click handler (see below).
-    // Set the id argument to become the active id in state
-    // (unless it already is, in which case we should reset
-    // the currently active square id back to initial state).
     return (id!=activeSquare) ? setActiveSquare(id): setActiveSquare(null);
   };
 
@@ -42,11 +34,7 @@ export default function Squares() {
     <div className='widget-squares container'>
       <h2>Squares</h2>
       <div className='squares'>
-        {
-          // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
-          // We might say: "it works, though!" But if the list of squares is not state,
-          // we could never add squares, change squares or remove squares in the future. Fix!
-          squares.map(id =>
+        {squares.map(id =>
             <div
               id={id}
               key={id}
